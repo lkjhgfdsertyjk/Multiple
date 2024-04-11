aac9cf5545fc25ef54c5bd78d184972b669d1c69
NIGHT <48613516+lkjhgfdsertyjk@users.noreply.github.com>
2024-04-11 19:07:23 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create subfolders
+mkdir -p tools targets
+
+# Install Sublist3r
+echo "Installing Sublist3r..."
+cd tools/
+git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r
+sudo pip install -r requirements.txt
+sudo pip install requests
+sudo pip install dnspython
+sudo pip install argparse
+
+# Install Eyewitness
+echo "Installing Eyewitness..."
+cd ..
+git clone https://github.com/RedSiege/EyeWitness.git
+cd EyeWitness/Python/setup
+./setup.sh
+cd ~
